539ba921602cad87bb171514954bff0816a7f466
服部元志 <hatowork.7979@gmail.com>
2023-10-20 18:46:32 +0900

    seedファイルの編集 sample_articlesの削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 3.times do |n|
+#   user = User.create!(
+#     email: "user#{format("%03d", n + 1)}@test.com",
+#     password: "test1234"
+#   )
+#   50.times do |s|
+#     user.articles.create!(
+#       title: "No.#{s + 1}:user#{format("%03d", n + 1)}の記事",
+#       content: "No.#{s + 1}:user#{format("%03d", n + 1)}の本文"
+#     )
+#   end
+# end
+
+3.times do |n|
+  email = "user#{format("%03d", n + 1)}@test.com"
+  user = User.find_or_create_by!(email: email) do |user|
+    user.password = "test1234"
+  end
+  50.times do |s|
+    title = "No.#{s + 1}:user#{format("%03d", n + 1)}の記事"
+    content = "No.#{s + 1}:user#{format("%03d", n + 1)}の本文"
+    user.articles.find_or_create_by!(title: title, content: content)
+  end
+end
