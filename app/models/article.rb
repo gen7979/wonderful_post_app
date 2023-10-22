c9b1a1e878e47870562aa96bd52ea3e4522b9594
服部元志 <hatowork.7979@gmail.com>
2023-10-22 20:15:50 +0900

    tag_articles追加 アソシエーションを追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,7 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   belongs_to :user
+
+  has_many :tag_articles
+  has_many :tags, through: :tag_articles
 end
